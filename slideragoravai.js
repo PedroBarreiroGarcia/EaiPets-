5bfb760f71a3d178da2bdf75a42d7a7911751e5e
Pedro Barreiro Garcia de Souza <125369614+PedroBarreiroGarcia@users.noreply.github.com>
2023-04-20 17:47:29 -0300

    ok

--- slideragoravai.js
+++ slideragoravai.js
@@ -10,3 +10,51 @@ function nextImage(){
     }
     document.getElementById("radio" + count).checked = true;
 }
+
+function show_cachorros() {
+    document.getElementById("camada_cachorros").style.visibility="visible";
+    document.getElementById("camada_gatos").style.visibility="hidden";
+    document.getElementById("camada_passaros").style.visibility="hidden";
+    document.getElementById("camada_peixes").style.visibility="hidden";
+    document.getElementById("camada_outros_pets").style.visibility="hidden";
+}
+
+function show_gatos() {
+    document.getElementById("camada_cachorros").style.visibility="hidden";
+    document.getElementById("camada_gatos").style.visibility="visible";
+    document.getElementById("camada_passaros").style.visibility="hidden";
+    document.getElementById("camada_peixes").style.visibility="hidden";
+    document.getElementById("camada_outros_pets").style.visibility="hidden";
+}
+
+function show_passaros() {
+    document.getElementById("camada_cachorros").style.visibility="hidden";
+    document.getElementById("camada_gatos").style.visibility="hidden";
+    document.getElementById("camada_passaros").style.visibility="visible";
+    document.getElementById("camada_peixes").style.visibility="hidden";
+    document.getElementById("camada_outros_pets").style.visibility="hidden";
+}
+
+function show_peixes() {
+    document.getElementById("camada_cachorros").style.visibility="hidden";
+    document.getElementById("camada_gatos").style.visibility="hidden";
+    document.getElementById("camada_passaros").style.visibility="hidden";
+    document.getElementById("camada_peixes").style.visibility="visible";
+    document.getElementById("camada_outros_pets").style.visibility="hidden";
+}
+
+function show_outros_pets() {
+    document.getElementById("camada_cachorros").style.visibility="hidden";
+    document.getElementById("camada_gatos").style.visibility="hidden";
+    document.getElementById("camada_passaros").style.visibility="hidden";
+    document.getElementById("camada_peixes").style.visibility="hidden";
+    document.getElementById("camada_outros_pets").style.visibility="visible";
+}
+
+function someTudo(){
+    document.getElementById("camada_cachorros").style.visibility="hidden";
+    document.getElementById("camada_gatos").style.visibility="hidden";
+    document.getElementById("camada_passaros").style.visibility="hidden";
+    document.getElementById("camada_peixes").style.visibility="hidden";
+    document.getElementById("camada_outros_pets").style.visibility="hidden";
+}
